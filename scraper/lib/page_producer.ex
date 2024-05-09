d66d964200c3e1b9ada3de999423f8c6bf7ec75a
charlieroth <charlieroth4@gmail.com>
2024-05-09 19:38:44 +0200

    Expose function in PageProducer to 'cast' pages to be scraped by PageConsumer

--- scraper/lib/page_producer.ex
+++ scraper/lib/page_producer.ex
@@ -2,6 +2,10 @@ defmodule PageProducer do
   use GenStage
   require Logger
 
+  def scrape_pages(pages) when is_list(pages) do
+    GenStage.cast(__MODULE__, {:pages, pages})
+  end
+
   def start_link(_args) do
     initial_state = []
     GenStage.start_link(__MODULE__, initial_state, name: __MODULE__)
@@ -16,5 +20,9 @@ defmodule PageProducer do
     Logger.info("PageProducer received demand for #{demand} pages")
     events = []
     {:noreply, events, state}
+  end
+
+  def handle_cast({:pages, pages}, state) do
+    {:noreply, pages, state}
   end
 end
