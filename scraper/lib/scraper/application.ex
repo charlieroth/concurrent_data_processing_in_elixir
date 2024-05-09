b8b3bb950e4bd353ac050a90703a26ed0238f9d2
charlieroth <charlieroth4@gmail.com>
2024-05-09 20:20:57 +0200

    Scale up OnlinePageProducerConsumer stage

--- scraper/lib/scraper/application.ex
+++ scraper/lib/scraper/application.ex
@@ -6,8 +6,24 @@ defmodule Scraper.Application do
   @impl true
   def start(_type, _args) do
     children = [
+      {Registry, keys: :unique, name: ProducerConsumerRegistry},
       PageProducer,
-      OnlinePageProducerConsumer,
+      %{
+        id: "online_page_producer_consumer_1",
+        start: {
+          OnlinePageProducerConsumer,
+          :start_link,
+          ["online_page_producer_consumer_1"]
+        }
+      },
+      %{
+        id: "online_page_producer_consumer_2",
+        start: {
+          OnlinePageProducerConsumer,
+          :start_link,
+          ["online_page_producer_consumer_2"]
+        }
+      },
       PageConsumerSupervisor
     ]
 
