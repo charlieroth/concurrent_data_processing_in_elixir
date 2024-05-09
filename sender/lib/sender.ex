c00d8c128dfb4eb9eb5b3fa138804bf9ac251b8d
charlieroth <charlieroth4@gmail.com>
2024-05-09 17:49:21 +0200

    Handling a async stream of Tasks with Task.Supervisor

--- sender/lib/sender.ex
+++ sender/lib/sender.ex
@@ -1,18 +1,25 @@
 defmodule Sender do
-  @moduledoc """
-  Documentation for `Sender`.
-  """
+  def notify_all(emails) do
+    # Creates an async stream of %Task{} under the
+    # EmailTaskSupervisor which are not linked so that
+    # when the below send_email/1 raises, it does not
+    # crash the supervising process but instead captures
+    # the %Task{} exit and returns it as a meaninful result
+    Sender.EmailTaskSupervisor
+    |> Task.Supervisor.async_stream_nolink(
+      emails,
+      &send_email/1
+    )
+    |> Enum.to_list()
+  end
 
-  @doc """
-  Hello world.
+  def send_email("konnichiwa@world.com" = email) do
+    raise "Oops, couldn't send emailt to #{email}"
+  end
 
-  ## Examples
-
-      iex> Sender.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def send_email(email) do
+    Process.sleep(3000)
+    IO.puts("Email to #{email} sent")
+    {:ok, "email_sent"}
   end
 end
