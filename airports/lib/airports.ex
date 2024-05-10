f83982c8c3eb83e75ad303e4ca37fe66e1664eb3
charlieroth <charlieroth4@gmail.com>
2024-05-10 20:44:15 +0200

    Parse CSV with Flow and File.Stream

--- airports/lib/airports.ex
+++ airports/lib/airports.ex
@@ -1,6 +1,24 @@
 defmodule Airports do
   alias NimbleCSV.RFC4180, as: CSV
 
+  def open_airports() do
+    airports_csv()
+    |> File.stream!()
+    |> Flow.from_enumerable()
+    |> Flow.map(fn row ->
+      [row] = CSV.parse_string(row, skip_headers: false)
+
+      %{
+        id: Enum.at(row, 0),
+        type: Enum.at(row, 2),
+        name: Enum.at(row, 3),
+        country: Enum.at(row, 8)
+      }
+    end)
+    |> Flow.reject(&(&1.type == "closed"))
+    |> Enum.to_list()
+  end
+
   def airports_csv() do
     Application.app_dir(:airports, "/priv/airports.csv")
   end
