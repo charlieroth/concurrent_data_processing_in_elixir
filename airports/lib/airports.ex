27a2e1b6abf319b14951bf5c85545d036b463162
charlieroth <charlieroth4@gmail.com>
2024-05-10 20:33:12 +0200

    finish initial setup for airports project

--- airports/lib/airports.ex
+++ airports/lib/airports.ex
@@ -1,18 +1,7 @@
 defmodule Airports do
-  @moduledoc """
-  Documentation for `Airports`.
-  """
+  alias NimbleCSV.RFC4180, as: CSV
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Airports.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def airports_csv() do
+    Application.app_dir(:airports, "/priv/airports.csv")
   end
 end
