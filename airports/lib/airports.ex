b8baee198a5007a24a8f2f70cda3c85f5e3118a8
charlieroth <charlieroth4@gmail.com>
2024-05-10 21:44:19 +0200

    Getting snapshots from slow-running or infinite flows

--- airports/lib/airports.ex
+++ airports/lib/airports.ex
@@ -2,8 +2,14 @@ defmodule Airports do
   alias NimbleCSV.RFC4180, as: CSV
 
   def open_airports() do
+    window = Flow.Window.trigger_every(Flow.Window.global(), 1000)
+
     airports_csv()
     |> File.stream!()
+    |> Stream.map(fn event ->
+      Process.sleep(Enum.random([0, 0, 0, 1]))
+      event
+    end)
     |> Flow.from_enumerable()
     |> Flow.map(fn row ->
       [row] = CSV.parse_string(row, skip_headers: false)
@@ -16,19 +22,22 @@ defmodule Airports do
       }
     end)
     |> Flow.reject(&(&1.type == "closed"))
-    |> Flow.partition(key: {:key, :country})
+    |> Flow.partition(window: window, key: {:key, :country})
     |> Flow.group_by(& &1.country)
-    |> Flow.on_trigger(fn airports_by_country ->
-      country_counts =
-        Enum.map(airports_by_country, fn {country, data} ->
-          {country, Enum.count(data)}
-        end)
+    |> Flow.on_trigger(fn acc, _partition_info, {_type, _id, trigger} ->
+      # Show progress in IEx
+      events =
+        acc
+        |> Enum.map(fn {country, data} -> {country, Enum.count(data)} end)
+        |> IO.inspect(label: inspect(self()))
 
-      {country_counts, airports_by_country}
+      case trigger do
+        :done -> {events, acc}
+        {:every, 1000} -> {[], acc}
+      end
     end)
-    |> Flow.take_sort(10, fn {_, a}, {_, b} -> a > b end)
-    |> Enum.to_list()
-    |> List.flatten()
+    |> Enum.sort(fn {_, a}, {_, b} -> a > b end)
+    |> Enum.take(10)
   end
 
   def airports_csv() do
